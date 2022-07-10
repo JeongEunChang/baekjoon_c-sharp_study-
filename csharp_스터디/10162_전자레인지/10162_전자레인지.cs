4afa2c924d6570320ac89aae0a49730b56deb9c3
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-07-10 18:12:25 +0900

    2022.07.10
    + 10162(전자레인지) -> 성공

---
+++ csharp_스터디/10162_전자레인지/10162_전자레인지.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace study
+{
+    class MicrowaveOven
+    {
+        static void Main(string[] args)
+        {
+            int TimeInput = int.Parse(ReadLine());
+            int A = 0;
+            int B = 0;
+            int C = 0;
+
+            if (TimeInput % 10 != 0)
+            {
+                WriteLine(-1);
+                return;
+            }
+
+            else
+            {
+                if (TimeInput >= 300)
+                {
+                    A = TimeInput / 300;
+                    TimeInput %= 300; 
+                }
+
+                if (TimeInput >= 60)
+                {
+                    B = TimeInput / 60;
+                    TimeInput %= 60;
+                }
+
+                    C = TimeInput / 10;
+            }
+
+            WriteLine("{0} {1} {2}", A, B, C);
+        }
+    }
+}
