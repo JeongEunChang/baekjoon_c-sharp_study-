ab4649cddf683fede75258f3daf14117622610ba
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-07-23 05:36:14 +0900

    2022.07.23
    + 2816(디지털 티비) -> 성공

---
+++ csharp_스터디/2816_디지털 티비/2816_디지털 티비.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using static System.Console;
+
+namespace study
+{
+    class Digital_TV
+
+    {
+        static void Main(string[] args)
+        {
+            string Input1 = ReadLine();
+            int ChannelNum = int.Parse(Input1);
+            string[] ChannelArr = new string[ChannelNum];
+
+            for (int i = 0; i < ChannelNum; i++)
+            {
+                string Input2 = ReadLine();
+                ChannelArr[i] = Input2;
+            }
+
+            for (int i = 0; i < ChannelNum; i++)
+            {
+                if (ChannelArr[i] == "KBS1")
+                {
+                    for (int j = i; j > 0; j--)
+                    {
+                        string tmp = ChannelArr[j];
+                        ChannelArr[j] = ChannelArr[j - 1];
+                        ChannelArr[j - 1] = tmp;
+                        Write('4');
+                    }
+
+                    break;
+                }
+
+                Write('1');
+            }
+
+            if (ChannelArr[1] == "KBS2")
+            {
+                return;
+            }
+
+            for (int i = 0; i < ChannelNum; i++)
+            {
+                if (ChannelArr[i] == "KBS2")
+                {
+                    for (int j = i; j > 1; j--)
+                    {
+                        Write('4');
+                    }
+
+                    break;
+                }
+
+                Write('1');
+            }
+        }
+    }
+}
